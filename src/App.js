06c56656671f3682086e0b39dee66ed384260e38
commGom <hyunwoo921129@gmail.com>
2021-12-20 22:56:35 +0900

    Spring React 통신

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import Member from './components/Member';
+import { Link, Route, Routes } from 'react-router-dom';
+// http://localhost:8080/
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Link to="/member">회원목록보기</Link>
+      <Routes>
+        <Route path="/member" element={<Member></Member>} />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/Member.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import axios from 'axios';
+const Member = () => {
+  const data = axios({
+    url: 'http://localhost:8080/json/exam',
+    method: 'get',
+
+  });
+  data.then(function (result) {
+    console.log(result);
+    const memberInfo = result.data;
+    const count = document.querySelector("span");
+    count.innerText = memberInfo.count;
+    const memberList = memberInfo.list;
+    const tbody = document.querySelector("tbody");
+    let tr = "";
+    memberList.forEach(member => {
+      tr += `
+      <tr>
+        <td>${member.name}</td>
+        <td>${member.userId}</td>
+        <td>${member.userPassword}</td>
+        </tr>
+      `;
+    });
+    tbody.innerHTML = tr;
+  });
+  return (
+    <div>
+      <h1>회원 목록 : <span id="count"></span></h1>
+
+      <table border="1">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>UserId</th>
+            <th>UserPassword</th>
+          </tr>
+        </thead>
+        <tbody>
+
+        </tbody>
+
+      </table>
+    </div>
+  );
+};
+
+export default Member;
